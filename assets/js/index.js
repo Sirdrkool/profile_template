b662f87dcb715a803862670060b45816dbc948bf
Ishaya Tiseh <161091725+Sirdrkool@users.noreply.github.com>
2025-05-19 13:55:26 +0100

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+const hiddenMenu = document.querySelector(".hiddenMenu");
+function ToggleMenu(){
+    hiddenMenu.classList.toggle("hiddenMenuOpen");
+}
+
+
+
+// document.addEventListener('scroll', () => {
+//     const videoPlayer = document.querySelector('.background-clip');
+//     if(window.scrollY > 20){
+//         videoPlayer.setAttribute = 'autoplay';
+//     }else{
+//         videoPlayer.setAttribute = 'muted';
+//     }
+// });
